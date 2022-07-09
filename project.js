39a2320ef84b2b1dbeb968d6a6ed96ffeef892f5
aditiya <aditiyaramadhani12@gmail.com>
2022-07-09 10:48:40 +0700

    first commit

--- project.js
+++ project.js
@@ -1,134 +1,183 @@
-let projects = [];
+function getProjects() {
+    let a = localStorage.getItem("list_project"); // get data from local storage
+    let isArrayEmpty = (x) => (x === null ? [] : JSON.parse(x)); // check if local storage is empty
+    return isArrayEmpty(a); // return array
+}
 
-function addProject() {
-    let title = document.getElementById("input-project-name").value;
-    let content = document.getElementById("input-description").value;
+window.onload = () => renderProjects(); // render projects when page is loaded
+function getDateDifference(startDate, endDate) {
+    // get date difference
+    if (startDate > endDate) {
+        // check if start date is greater than end date
+        console.error("Start date must be before end date"); // throw error
+        return null; // return null
+    }
+    let startYear = startDate.getFullYear(); // get start year
+    let startMonth = startDate.getMonth(); // get start month
+    let startDay = startDate.getDate(); // get start day
 
-    let startdate = document.getElementById("input-start-date").value;
-    let stopdate = document.getElementById("input-end-date").value;
+    let endYear = endDate.getFullYear(); // get end year
+    let endMonth = endDate.getMonth(); // get end month
+    let endDay = endDate.getDate(); // get end day
 
-    let image = document.getElementById("input-project-image").files[0];
-    image = URL.createObjectURL(image);
+    let february =
+        (endYear % 4 == 0 && endYear % 100 != 0) || endYear % 400 == 0
+            ? 29
+            : 28; // check if end year is leap year
+    let daysOfMonth = [31, february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; // get days of month
 
-    let ceklis = Array.from(
-        document.querySelectorAll("input[type=checkbox]:checked")
-    ).map((item) => item.value);
+    let startDateNotPassedInEndYear =
+        endMonth < startMonth || (endMonth == startMonth && endDay < startDay); // check if start date is not passed in end year
+    let years = endYear - startYear - (startDateNotPassedInEndYear ? 1 : 0); // get years
+    let months =
+        (12 + endMonth - startMonth - (endDay < startDay ? 1 : 0)) % 12; // get months
 
-    let project = {
-        author: "Aditiya Ramadhani",
-        title,
-        startdate,
-        stopdate,
-        content,
-        image,
-        ceklis,
+    let days =
+        startDay <= endDay
+            ? endDay - startDay
+            : daysOfMonth[(12 + endMonth - 1) % 12] - startDay + endDay; // get days
+
+    return {
+        years: years,
+        months: months,
+        days: days,
     };
+}
 
-    projects.push(project);
+function addProjects() {
+    let title = document.getElementById("input-project-name").value; // get title from input
+    let startDate = new Date(document.getElementById("input-start-date").value); // get start date from input
+    let endDate = new Date(document.getElementById("input-end-date").value); // get end date from input
+    let lengthDate = getDateDifference(startDate, endDate); // get date difference
+    let description = document.getElementById("input-description").value; // get description from input
+    let image = document.getElementById("upload-image").files[0]; // get image from input
+
+    let logoTechlist = []; // create array for logo tech
+    if (document.getElementById("node-js").checked) {
+        logoTechlist.push("node-js");
+    }
+    if (document.getElementById("react-js").checked) {
+        logoTechlist.push("react-js");
+    }
+    if (document.getElementById("next-js").checked) {
+        logoTechlist.push("next-js");
+    }
+    if (document.getElementById("typeScript-js").checked) {
+        logoTechlist.push("typeScript-js");
+    }
+    const dateFormatOptions = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    }; // set date format
+
+    image = URL.createObjectURL(image); // create image url
+    let project = {
+        id: Math.floor(Math.random() * 1000000),
+        title: title,
+        startDate: startDate.toLocaleDateString("id-ID", dateFormatOptions),
+        endDate: endDate.toLocaleDateString("id-ID", dateFormatOptions),
+        lengthDate: lengthDate.months,
+        description: description,
+        logoTechlist: logoTechlist,
+        image: image,
+    }; // create project object
+    let data = getProjects(); // get data from local storage
+    data.push(project); // push project to data
+    localStorage.setItem("list_project", JSON.stringify(data)); // set data to local storage
+    renderProjects();
+}
+
+function checkTechLogoifExists(list) {
+    let logoTech = "";
+    if (list.includes("node-js")) {
+        logoTech += '<img src="../img/nodejs-icon.svg" alt="node-js">';
+    }
+    if (list.includes("react-js")) {
+        logoTech += '<img src="../img/react.svg" alt="react-js">';
+    }
+    if (list.includes("next-js")) {
+        logoTech += '<img src="../img/nextjs-icon.svg" alt="next-js">';
+    }
+    if (list.includes("typeScript-js")) {
+        logoTech +=
+            '<img src="../img/typescript-icon.svg" alt="typescript-js">';
+    }
+    return logoTech;
+}
+
+function deleteProject(id) {
+    let data = JSON.parse(localStorage.getItem("list_project")); // get data from local storage
+    const removeIndex = data.findIndex((item) => item.id === id); // get index of project
+    data.splice(removeIndex, 1); // remove project from data
+    localStorage.setItem("list_project", JSON.stringify(data)); // set data to local storage
     renderProjects();
 }
 
 function renderProjects() {
-    lengthData = projects.length;
-    let projectContainer = document.getElementById("contents");
-    projectContainer.innerHTML = firstProjectContent();
+    let projectsContainer = document.getElementById("contents-projects"); // get projects container
 
-    let i = 0;
-    for (i; i < lengthData; i++) {
-        let icons = projects[i].ceklis.map(
-            (value) => `<img src="assets/${value}">`
-        );
-
-        projectContainer.innerHTML +=
-            `
-        <div class="project-item">
-        <div class="my-project-img">
-          <img src="${projects[i].image}" alt="" />
-        </div>
-        <div class="detail-project">
-          <h3>
-            <a href="my-project-detail.html" target="_blank">${
-                projects[i].title
-            }</a>
-          </h3>
-          <div class="detail-project-content">
-            Duration : ${durationTime(
-                projects[i].startdate,
-                projects[i].stopdate
-            )}
-          </div>
-          <p>${projects[i].content}</p>
-          
-          <div class="logo">
-          <div class="nodesj">
-          ` +
-            icons +
-            `
-          </div>
-          </div>
-          <div class="btn-group">
-            <button class="btn-edit">edit</button>
-            <button class="btn-delete">delete</button>
-          </div>
-        </div>
+    projectsContainer.innerHTML = ""; // clear projects container
+    let projects = getProjects(); // get projects from local storage
+    projects.forEach((data) => {
+        projectsContainer.innerHTML += `
+    <div class="grid-item" >
+      <img src="${data.image}" alt="">
+      <div class="title">
+        <h2><a href="project-detail.html?id=${data.id}">${data.title}</a></h2>
+        <p>durasi : ${data.lengthDate} bulan</p>
       </div>
-      `;
-    }
-
-    // Non Looping 2x
-    function firstProjectContent() {
-        return `<div class="project-item">
-    <div class="my-project-img">
-      <img src="assets/my-project-img-detail.png" alt="cover" />
-    </div>
-    <div class="detail-project">
-      <h4>
-        <a href="my-project-detail.html" target="_blank">Dumbways Mobile App - 2021</a>
-      </h4>
-      <div class="detail-project-content">
-        Durasi : 3 Bulan
-      </div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione magnam minima in culpa itaque deleniti unde
-        quae aperiam ex architecto, blanditiis eveniet.
-      </p>
-      <div class="logo">
-        <div class="nodejs">
-          <img src="assets/node-js.png" alt="ns" id="nss">
-        </div>
-        <div class="reactjs">
-          <img src="assets/reactjs.png" alt="rs" id="rss">
-        </div>
-        <div class="javascript">
-          <img src="assets/js.png" alt="js" id="jss">
-        </div>
-        <div class="typescript">
-          <img src="assets/typescript.png" alt="ts" id="tss">
-        </div>
-      </div>
-      <div class="btn-group">
-        <button class="btn-edit">edit</button>
-        <button class="btn-delete">delete</button>
-      </div>
-    </div>
-  </div>`;
-    }
+              <p> ${data.description} </p>
+              <div class="tech-icon">
+                  ${checkTechLogoifExists(data.logoTechlist)}
+              </div>
+              <div class="btn-group">
+                  <button class="btn-edit" type="button" onclick="">edit</button>
+                  <button class="btn-delete" type="button" onclick="deleteProject(${
+                      data.id
+                  })">delete</button>
+              </div>
+          </div>`;
+    });
 }
 
-function durationTime(startdate, stopdate) {
-    // Convert Start - End Date to Days
-    let newStartDate = new Date(startdate);
-    let newEndDate = new Date(stopdate);
-    let duration = Math.abs(newStartDate - newEndDate);
-
-    let day = Math.floor(duration / (1000 * 60 * 60 * 24));
-
-    if (day < 30) {
-        return day + ` days `;
-    } else {
-        let diffMonths = Math.ceil(duration / (1000 * 60 * 60 * 24 * 30));
-        if (diffMonths >= 1) {
-            return diffMonths + ` month `;
-        }
-    }
+function getProjectDetail() {
+    let params = new URLSearchParams(location.search); // get params from url
+    let id = params.get("id"); // get id from params
+    let a = localStorage.getItem("list_project"); // get data from local storage
+    let isArrayEmpty = (x) => (x === null ? [] : JSON.parse(x)); // check if array is empty
+    let projects = isArrayEmpty(a); // get projects from local storage
+    return projects.find((x) => x.id == id); // get project from projects
 }
+
+window.onload = () => {
+    let data = getProjectDetail(); // get project detail
+    let projectsDetailContainer = document.getElementById("detail-project"); // get projects detail container
+    projectsDetailContainer.innerHTML += `
+  <h1 class="header">${data.title}</h1>
+      <div class="project">
+          <img src="${data.image}" class="image-project">
+          <div class="item">
+              <h2 class="sub-title">Duration</h2>
+              <div class="duration">
+                  <p><i class="fa-solid fa-calendar-days"></i> ${
+                      data.startDate
+                  } - ${data.endDate}</p>
+                  <p><i class="fa-solid fa-clock-rotate-left"></i> ${
+                      data.lengthDate
+                  } Months</p>
+              </div>
+              <h2 class="sub-title">Technologies</h2>
+              <div class="tech-icon" id="tech-icon-project-detail">
+                  <div class="">
+                      <div class="item-icon">
+                          ${checkTechLogoifExists(data.logoTechlist)}
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div class="project-description">
+          <p>${data.description}</p>
+      </div>`;
+};
